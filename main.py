2b45014d02c54db7725bfacce75aacc2f0f820c0
Juani <juanignacioch4@gmail.com>
2026-03-03 20:52:38 -0300

    chore: users_logic para operaciones con usuario con csv

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, HTTPException, Query
 from models import User, ItemArray, Error, UserCreationDTO
 from exceptions import UserNotFoundException
 from recommendation import get_k_recommendations
+from users_logic import get_csv_user, get_next_user_id
 
 app = FastAPI(title="Sistema Recomendador - Ciencia de Datos 2025")
 CSV_FILE = "database/usuarios.csv"
@@ -31,43 +32,6 @@ def init_db():
 async def startup_event():
     init_db()
 
-# ---------- HELPERS ----------
-def get_user_or_404(user_id: int) -> User:
-    df = pd.read_csv(CSV_FILE)
-    user_row = df[df["id"] == user_id]
-
-    if user_row.empty:
-        raise UserNotFoundException(user_id)
-
-    row = user_row.iloc[0]
-    return User(
-        id=int(row["id"]),
-        username=row["username"],
-        attributes={
-            "open_word_action_preference": float(row["open_word_action_preference"]),
-            "fps_preference": float(row["fps_preference"]),
-            "survival_preference": float(row["survival_preference"]),
-            "action_rpg_preference": float(row["action_rpg_preference"]),
-            "linear_action_adventure_preference": float(row["linear_action_adventure_preference"]),
-        },
-    )
-
-def get_next_user_id() -> int:
-    df = pd.read_csv(CSV_FILE)
-
-    if df.empty or "id" not in df.columns:
-        return 1
-
-    ids = pd.to_numeric(df["id"], errors="coerce").dropna().astype(int)
-    if ids.empty:
-        return 1
-
-    used = set(ids.tolist())
-    new_id = 1
-    while new_id in used:
-        new_id += 1
-    return new_id
-
 # ----- ENDPOINTS -----
 @app.post("/user", response_model=User, tags=["CRUD: Usuario"])
 async def create_user(payload: UserCreationDTO):
@@ -93,17 +57,17 @@ async def create_user(payload: UserCreationDTO):
 
 @app.get("/user/{userId}", response_model=User, tags=["CRUD: Usuario"])
 async def get_user(userId: int):
-    return get_user_or_404(userId)
+    return get_csv_user(userId)
 
 @app.get("/user/{userId}/recommend", response_model=ItemArray, tags=["Sistema Recomendador"])
 async def get_recommendations(userId: int, k: int = Query(5)):
-    user = get_user_or_404(userId)
+    user = get_csv_user(userId)
     items = get_k_recommendations(user.id, k)
     return ItemArray(items=items)
 
 @app.post("/user/{userId}/preference/{itemId}", tags=["Sistema Recomendador"])
 async def add_preference(userId: int, itemId: int, ranking: int):
-    user = get_user_or_404(userId)
+    user = get_csv_user(userId)
 
     df_p = pd.read_csv(PREF_CSV)
     new_pref = {"userId": user.id, "itemId": itemId}
